22b71c2e35e8fddd1c1ba1d96107f034ad704239
Rica <pudimdefrutas@gmail.com>
2020-03-18 17:52:26 -0300

    Drag n drop now accounts for mouse offset

--- project/test/Battle/Reagent.gd
+++ project/test/Battle/Reagent.gd
@@ -3,7 +3,8 @@ extends Control
 var type = "reagent"
 
 var can_drag = false
+var drag_offset = Vector2(0,0)
 
 func _process(_delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_drag:
-		rect_position = get_global_mouse_position()
+		rect_position = get_global_mouse_position() + drag_offset
--- project/test/Battle/ReagentCollision.gd
+++ project/test/Battle/ReagentCollision.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 onready var shape = $CollisionShape2D.shape
-
+onready var reagent = get_parent()
 
 func _input(event):
 	if event is InputEventMouseButton:
 		var mouse_pos = get_local_mouse_position()
 		if mouse_pos.x >= -shape.extents.x and mouse_pos.x <= shape.extents.x and \
 		   mouse_pos.y >= -shape.extents.y and mouse_pos.y <= shape.extents.y:
-			get_parent().can_drag = event.pressed
+			reagent.can_drag = event.pressed
+			if reagent.can_drag:
+				reagent.drag_offset = -get_local_mouse_position()
 

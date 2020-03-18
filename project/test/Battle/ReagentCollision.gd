deb867e5633ec6e96e1de215bb55883b19218f71
Rica <pudimdefrutas@gmail.com>
2020-03-18 18:05:05 -0300

    reagents move to target position

--- project/test/Battle/ReagentCollision.gd
+++ project/test/Battle/ReagentCollision.gd
@@ -8,7 +8,7 @@ func _input(event):
 		var mouse_pos = get_local_mouse_position()
 		if mouse_pos.x >= -shape.extents.x and mouse_pos.x <= shape.extents.x and \
 		   mouse_pos.y >= -shape.extents.y and mouse_pos.y <= shape.extents.y:
-			reagent.can_drag = event.pressed
-			if reagent.can_drag:
+			reagent.is_drag = event.pressed
+			if reagent.is_drag:
 				reagent.drag_offset = -get_local_mouse_position()
 

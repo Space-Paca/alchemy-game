d42962f8fc4bb815931f124b0af9fee2794a98b4
Rica <pudimdefrutas@gmail.com>
2020-03-19 15:51:10 -0300

    Still trying to fix case problem

--- project/test/util/Hand/Hand.gd
+++ project/test/util/Hand/Hand.gd
@@ -2,6 +2,8 @@ tool
 extends Node2D
 
 const HANDSLOT = preload("res://test/Battle/Hand/HandSlot.tscn")
+const H_MARGIN = 40
+const V_MARGIN = 25
 
 export var size = 10
 
@@ -10,6 +12,7 @@ onready var BG = $TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Grid.rect_position = Vector2(H_MARGIN, V_MARGIN)
 	setHand(8)
 
 
@@ -19,4 +22,5 @@ func setHand(slots):
 	Grid.columns = slots/2
 	for i in range(slots):
 		Grid.add_child(HANDSLOT.instance())
-	BG.size.x = Grid.get_child(1).size.x * size/2
+	BG.rect_size.x = Grid.get_child(1).rect_size.x * slots/2 + 2*V_MARGIN
+	BG.rect_size.y = Grid.get_child(1).rect_size.y * 2 + 2*V_MARGIN

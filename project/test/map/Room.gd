b5bd379fca159bd5dfcf08affd37e87d0292c525
ArthurTui <arthur.vb@gmail.com>
2020-03-19 17:36:35 -0300

    Change unexplored room border color

--- project/test/map/Room.gd
+++ project/test/map/Room.gd
@@ -24,30 +24,31 @@ func _draw():
 	var width = 4
 	var w2 = width / 2
 	var gap = 20
+	var color = Color.black if type == Type.EMPTY else Color.dimgray
 	
 	if entrance == N or (exits[N] and type == Type.EMPTY):
-		draw_line(Vector2(0 , w2), Vector2(SIZE.x / 2 - gap, w2), Color.black, width)
-		draw_line(Vector2(SIZE.x / 2 + gap, w2), Vector2(SIZE.x, w2), Color.black, width)
+		draw_line(Vector2(0 , w2), Vector2(SIZE.x / 2 - gap, w2), color, width)
+		draw_line(Vector2(SIZE.x / 2 + gap, w2), Vector2(SIZE.x, w2), color, width)
 	else:
-		draw_line(Vector2(0, w2), Vector2(SIZE.x, w2), Color.black, width)
+		draw_line(Vector2(0, w2), Vector2(SIZE.x, w2), color, width)
 	
 	if entrance == W or (exits[W] and type == Type.EMPTY):
-		draw_line(Vector2(w2, 0), Vector2(w2, SIZE.y / 2 - gap), Color.black, width)
-		draw_line(Vector2(w2, SIZE.y / 2 + gap), Vector2(w2, SIZE.y), Color.black, width)
+		draw_line(Vector2(w2, 0), Vector2(w2, SIZE.y / 2 - gap), color, width)
+		draw_line(Vector2(w2, SIZE.y / 2 + gap), Vector2(w2, SIZE.y), color, width)
 	else:
-		draw_line(Vector2(w2, 0), Vector2(w2, SIZE.y), Color.black, width)
+		draw_line(Vector2(w2, 0), Vector2(w2, SIZE.y), color, width)
 	
 	if entrance == E or (exits[E] and type == Type.EMPTY):
-		draw_line(Vector2(SIZE.x - w2, 0), Vector2(SIZE.x - w2, SIZE.y / 2 - gap), Color.black, width)
-		draw_line(Vector2(SIZE.x - w2, SIZE.y / 2 + gap), Vector2(SIZE.x - w2, SIZE.y), Color.black, width)
+		draw_line(Vector2(SIZE.x - w2, 0), Vector2(SIZE.x - w2, SIZE.y / 2 - gap), color, width)
+		draw_line(Vector2(SIZE.x - w2, SIZE.y / 2 + gap), Vector2(SIZE.x - w2, SIZE.y), color, width)
 	else:
-		draw_line(Vector2(SIZE.x - w2, 0), Vector2(SIZE.x - w2, SIZE.y), Color.black, width)
+		draw_line(Vector2(SIZE.x - w2, 0), Vector2(SIZE.x - w2, SIZE.y), color, width)
 
 	if entrance == S or (exits[S] and type == Type.EMPTY):
-		draw_line(Vector2(0, SIZE.y - w2), Vector2(SIZE.x / 2 - gap, SIZE.y - w2), Color.black, width)
-		draw_line(Vector2(SIZE.x / 2 + gap, SIZE.y - w2), Vector2(SIZE.x, SIZE.y - w2), Color.black, width)
+		draw_line(Vector2(0, SIZE.y - w2), Vector2(SIZE.x / 2 - gap, SIZE.y - w2), color, width)
+		draw_line(Vector2(SIZE.x / 2 + gap, SIZE.y - w2), Vector2(SIZE.x, SIZE.y - w2), color, width)
 	else:
-		draw_line(Vector2(0, SIZE.y - w2), Vector2(SIZE.x, SIZE.y - w2), Color.black, width)
+		draw_line(Vector2(0, SIZE.y - w2), Vector2(SIZE.x, SIZE.y - w2), color, width)
 
 
 func set_type(new_type : int):

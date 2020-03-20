214846b6c93b431215f81b7987c6fe8df5bd327d
Rica <pudimdefrutas@gmail.com>
2020-03-20 06:14:15 -0300

    Working database for reagents

--- project/test/reagents/ReagentManager.gd
+++ project/test/reagents/ReagentManager.gd
@@ -1,6 +1,31 @@
 extends Node
 
 const REAGENT = preload("res://test/reagents/Reagent.tscn")
+const DB_PATH = "res://database/ReagentsDB.json"
+var Data
+
+func ready():
+	if not Data:
+		get_data()
+
+func get_data():
+	var data_file = File.new()
+	if data_file.open(DB_PATH, File.READ) != OK:
+		push_error("Couldn't open reagents database file")
+		assert(false)
+	var data_text = data_file.get_as_text()
+	data_file.close()
+	var data_parse = JSON.parse(data_text)
+	if data_parse.error != OK:
+		push_error("Couldn't parse reagents database file")
+		assert(false)
+	Data = data_parse.result
 
 func create(type):
-	return REAGENT.instance()
+	if not Data:
+		get_data()
+		
+	var reagent = REAGENT.instance()
+	reagent.type = type
+	reagent.set_image(Data[type].image)
+	return reagent

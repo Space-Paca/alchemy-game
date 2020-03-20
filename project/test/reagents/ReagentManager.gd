541c23ccde7a403f3224a926a5f8bd5218c08e97
Rica <pudimdefrutas@gmail.com>
2020-03-20 06:43:04 -0300

    More robust database system

--- project/test/reagents/ReagentManager.gd
+++ project/test/reagents/ReagentManager.gd
@@ -1,28 +1,11 @@
 extends Node
 
 const REAGENT = preload("res://test/reagents/Reagent.tscn")
-const DB_PATH = "res://database/ReagentsDB.json"
+
+onready var DB = get_node("/root/DbManager")
 
 const TYPES = ["regular", "special", "harmless", "funky", "powerful", "tasty", \
 			   "marvellous"]
-var Data
-
-func ready():
-	if not Data:
-		get_data()
-
-func get_data():
-	var data_file = File.new()
-	if data_file.open(DB_PATH, File.READ) != OK:
-		push_error("Couldn't open reagents database file")
-		assert(false)
-	var data_text = data_file.get_as_text()
-	data_file.close()
-	var data_parse = JSON.parse(data_text)
-	if data_parse.error != OK:
-		push_error("Couldn't parse reagents database file")
-		assert(false)
-	Data = data_parse.result
 
 func random_type():
 	randomize()
@@ -30,14 +13,14 @@ func random_type():
 	return TYPES[1]
 
 func create(type):
-	if not Data:
-		get_data()
 	
 	if not TYPES.has(type):
 		push_error("Given type of reagent doesn't exist")
 		assert(false)
 	
+	var data = DB.get_data("reagents")
+	
 	var reagent = REAGENT.instance()
 	reagent.type = type
-	reagent.set_image(Data[type].image)
+	reagent.set_image(data[type].image)
 	return reagent

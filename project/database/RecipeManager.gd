d8af2c0490d0593142ab9e7db2af7ca32d477420
ArthurTui <arthur.vb@gmail.com>
2020-03-30 16:03:14 -0300

    Rework combination generation

--- project/database/RecipeManager.gd
+++ project/database/RecipeManager.gd
@@ -11,7 +11,10 @@ func _ready():
 		var file_name := (dir.get_next() as String)
 		while file_name != "":
 			if not dir.current_is_dir() and file_name.get_extension() == "tres":
-				recipes[file_name.rstrip(".tres")] = load(str(path, file_name))
+				var recipe := (load(str(path, file_name)) as Recipe)
+				if recipe.reagents.size() < 2:
+					print("RecipeManager: %a has less than 2 reagents" % file_name)
+				recipes[file_name.rstrip(".tres")] = recipe
 			file_name = dir.get_next()
 	else:
 		print("RecipeManager: An error occurred when trying to access the path.")
--- project/test/dungeon/Combination.gd
+++ project/test/dungeon/Combination.gd
@@ -1,8 +1,8 @@
 extends Node
 class_name Combination
 
-var c_grid_size : int
-var c_name : String
+var grid_size : int
+var recipe_name : String
 var matrix : Array
 
 func _ready():
@@ -10,18 +10,35 @@ func _ready():
 
 
 func create_from_recipe(recipe: Recipe):
-	c_grid_size = recipe.grid_size
-	c_name = recipe.name
+	grid_size = recipe.grid_size
+	recipe_name = recipe.name
+	var avaiable_positions := []
 	var elements := (recipe.reagents.duplicate() as Array)
-	
-	for i in range(c_grid_size * c_grid_size - elements.size()):
-		elements.append(null)
-	
 	elements.shuffle()
 	
-	for i in range(c_grid_size):
+	for i in range(grid_size):
 		var line = []
-		for j in range(c_grid_size):
-			line.append(elements.pop_front())
-		
+		for j in range(grid_size):
+			line.append(null)
+			avaiable_positions.append([i, j])
 		matrix.append(line)
+	
+	# Placing the first two reagents that guarantee grid size consistency
+	var pos1 : Array
+	var pos2 : Array
+	if randf() < .5:
+		pos1 = [0, randi() % grid_size]
+		pos2 = [grid_size - 1, randi() % grid_size]
+	else:
+		pos1 = [randi() % grid_size, 0]
+		pos2 = [randi() % grid_size, grid_size - 1]
+	matrix[pos1[0]][pos1[1]] = elements.pop_front()
+	matrix[pos2[0]][pos2[1]] = elements.pop_front()
+	avaiable_positions.erase(pos1)
+	avaiable_positions.erase(pos2)
+	
+	# Placing the rest
+	avaiable_positions.shuffle()
+	while not elements.empty():
+		var pos = avaiable_positions.pop_front()
+		matrix[pos[0]][pos[1]] = elements.pop_front()

7943ea2d80d95f3e2c14354304b163258af5c207
Rica <pudimdefrutas@gmail.com>
2020-03-31 19:17:25 -0300

    Attempt to fix reagent sticking in place

--- project/test/reagents/Reagent.gd
+++ project/test/reagents/Reagent.gd
@@ -12,9 +12,11 @@ var target_position = rect_position
 var type = "none"
 
 func _process(_delta):
+	if not Input.is_mouse_button_pressed(BUTTON_LEFT):
+		is_drag = false
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and is_drag:
 		rect_position = get_global_mouse_position() + drag_offset
-	elif (not can_drag or not is_drag) and target_position:
+	elif not is_drag and target_position:
 		if rect_position.distance_to(target_position) > 0:
 			rect_position += (target_position - rect_position)*.3
 			if (target_position - rect_position).length() < 3:
--- project/test/reagents/ReagentCollision.gd
+++ project/test/reagents/ReagentCollision.gd
@@ -5,26 +5,27 @@ onready var reagent = get_parent()
 
 func _input(event):
 	if event is InputEventMouseButton:
-		var mouse_pos = get_local_mouse_position()
-		if mouse_pos.x >= -shape.extents.x and mouse_pos.x <= shape.extents.x and \
-		   mouse_pos.y >= -shape.extents.y and mouse_pos.y <= shape.extents.y:
-			if reagent.can_drag:
-				reagent.is_drag = event.pressed
-			if reagent.is_drag:
-				reagent.drag_offset = -get_local_mouse_position()
-			else:
-				reagent.can_drag = false #Disable dragging until reagent returns to target position
-				var nearest_slot_area = null
-				for area in get_overlapping_areas():
-					if area.is_in_group("gridslot") or area.is_in_group("handslot"):
-						if not nearest_slot_area:
+		if event.pressed and reagent.can_drag:
+			var mouse_pos = get_local_mouse_position()
+			if mouse_pos.x >= -shape.extents.x and mouse_pos.x <= shape.extents.x and \
+			   mouse_pos.y >= -shape.extents.y and mouse_pos.y <= shape.extents.y:
+					reagent.is_drag = true
+					reagent.drag_offset = -get_local_mouse_position()
+		elif reagent.is_drag:
+			reagent.is_drag = false
+			var nearest_slot_area = null
+			for area in get_overlapping_areas():
+				if area.is_in_group("gridslot") or area.is_in_group("handslot"):
+					if not nearest_slot_area:
+						nearest_slot_area = area
+					else:
+						if self.global_position.distance_to(nearest_slot_area.global_position) > \
+						   self.global_position.distance_to(area.global_position):
 							nearest_slot_area = area
-						else:
-							if self.global_position.distance_to(nearest_slot_area.global_position) > \
-							   self.global_position.distance_to(area.global_position):
-								nearest_slot_area = area
-				if nearest_slot_area:
-					var slot = nearest_slot_area.get_parent()
-					if not slot.get_reagent():
-						slot.set_reagent(reagent)
+			if nearest_slot_area:
+				var slot = nearest_slot_area.get_parent()
+				if not slot.get_reagent():
+					slot.set_reagent(reagent)
+					if self.global_position.distance_to(nearest_slot_area.global_position) > 0:
+						reagent.can_drag = false
 

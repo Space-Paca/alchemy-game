29cd3ebe22642215233d924ff52585326c54a8e0
ArthurTui <arthur.vb@gmail.com>
2020-04-19 19:10:16 -0300

    Add loot table to encounters

--- project/database/Encounter.gd
+++ project/database/Encounter.gd
@@ -4,3 +4,31 @@ class_name Encounter
 export(bool) var is_boss = false
 export(int, 1, 3) var level = 1
 export(Array, String, "skeleton", "wolf", "robot") var enemies
+
+# Loot table
+export(Dictionary) var loot_table := {"common": 0, "uncommon": 0, "rare": 0,
+	"damaging": 0, "defensive": 0, "super_damaging": 0, "super_defensive": 0,
+	"healing": 0, "catalyst": 0}
+export(Array, float) var extra_loot_chance : Array
+
+
+func get_loot() -> Array:
+	var loot := []
+	var chance := 1
+	var extra_chance := extra_loot_chance.duplicate()
+	var pool := []
+	
+	for reagent in loot_table:
+		for i in range(loot_table[reagent]):
+			pool.append(reagent)
+	
+	while chance >= randf():
+		pool.shuffle()
+		loot.append(pool.front())
+		
+		if extra_chance.empty():
+			chance = 0
+		else:
+			chance = extra_chance.pop_front()
+	
+	return loot

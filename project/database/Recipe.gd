514c9bb5ef463bd79e545ad5b4b944b17dc29ea8
ArthurTui <arthur.vb@gmail.com>
2020-04-29 19:28:34 -0300

    Add all recipes

--- project/database/Recipe.gd
+++ project/database/Recipe.gd
@@ -3,6 +3,7 @@ class_name Recipe
 
 export(String) var name
 export(int) var grid_size
+export(int) var floor_sold_in
 export(Array, String) var reagents
 export(Array, String) var effects
 export(Array, Array) var effect_args

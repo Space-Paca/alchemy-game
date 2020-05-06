4bebea4fe02394c6b104083fec16d5dc45f4bc65
Rica <pudimdefrutas@gmail.com>
2020-05-06 18:45:42 -0300

    Added improved arts

--- project/game/battle/grid/GridSlot.gd
+++ project/game/battle/grid/GridSlot.gd
@@ -4,16 +4,12 @@ const ALPHA_SPEED = 3
 
 onready var reagent_hint = $MarginContainer/ReagentHint
 
-func _ready():
-	$FullImage.modulate.a = 0
-	$EmptyImage.modulate.a = 1
-
 func _process(delta):
 	if get_reagent():
 		$FullImage.modulate.a = min($FullImage.modulate.a + ALPHA_SPEED*delta, 1)
-		$EmptyImage.modulate.a = max($FullImage.modulate.a - ALPHA_SPEED*delta, 0)
+		$EmptyImage.modulate.a = max($EmptyImage.modulate.a - ALPHA_SPEED*delta, 0)
 	else:
-		$EmptyImage.modulate.a = min($FullImage.modulate.a + ALPHA_SPEED*delta, 1)
+		$EmptyImage.modulate.a = min($EmptyImage.modulate.a + ALPHA_SPEED*delta, 1)
 		$FullImage.modulate.a = max($FullImage.modulate.a - ALPHA_SPEED*delta, 0)
 
 func set_hint(reagent_name):

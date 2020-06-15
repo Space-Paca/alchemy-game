02d97fd81943038596ff3f012a4518fa9f85b222
ArthurTui <arthur.vb@gmail.com>
2020-06-15 19:19:19 -0300

    Add list with reagent buttons to be reused

---
+++ project/game/ui/ClickableReagentList.gd
@@ -0,0 +1,20 @@
+extends Control
+class_name ClickableReagentList
+
+signal reagent_pressed(reagent_name)
+
+onready var grid = $ScrollContainer/GridContainer
+
+
+func populate(reagent_array: Array):
+	for reagent_name in reagent_array:
+		var texture = ReagentDB.get_from_name(reagent_name).image
+		var button = TextureButton.new()
+		
+		button.texture_normal = texture
+		button.connect("pressed", self, "_on_reagent_pressed", [reagent_name])
+		grid.add_child(button)
+
+
+func _on_reagent_pressed(reagent_name: String):
+	emit_signal("reagent_pressed", reagent_name)

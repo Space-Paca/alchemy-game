f5f090d0476ca945dd794e345a62f563f15e105e
ArthurTui <arthur.vb@gmail.com>
2020-08-14 19:37:08 -0300

    Add legend to map nodes

--- project/game/map/MapNode.gd
+++ project/game/map/MapNode.gd
@@ -4,6 +4,7 @@ class_name MapNode
 signal pressed
 
 onready var button = $Button
+onready var legend = $Legend
 
 enum {EMPTY, ENEMY, ELITE, BOSS, SHOP, REST, SMITH, EVENT}
 
@@ -15,6 +16,8 @@ const IMAGES = [preload("res://assets/images/map/elementCircle.png"),
 		preload("res://assets/images/map/descanso.png"),
 		preload("res://assets/images/map/reagents.png"),
 		preload("res://assets/images/map/eventos.png")]
+const LEGEND = ["", "Enemy encounter", "Elite encounter", "Boss encounter",
+		"Shop", "Rest area", "Reagent smith", "Event"]
 
 var encounter : Encounter
 var is_leaf := true
@@ -38,6 +41,8 @@ func set_type(new_type:int):
 		encounter = EncounterManager.get_random_elite_encounter()
 	elif type == BOSS:
 		encounter = EncounterManager.get_random_boss_encounter()
+	
+	legend.text = LEGEND[type]
 
 
 func should_autoreveal() -> bool:
@@ -46,3 +51,11 @@ func should_autoreveal() -> bool:
 
 func _on_Button_pressed():
 	emit_signal("pressed")
+
+
+func _on_Button_mouse_entered():
+	$Legend.show()
+
+
+func _on_Button_mouse_exited():
+	$Legend.hide()

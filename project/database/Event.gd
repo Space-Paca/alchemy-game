9af01e5d73bd51e4c25c7265dcd4c74f6dd2b7dc
ArthurTui <arthur.vb@gmail.com>
2020-10-21 15:35:19 -0300

    [wip] Events

--- project/database/Event.gd
+++ project/database/Event.gd
@@ -3,7 +3,9 @@ class_name Event
 
 enum Type {LUCK, CHALLENGE, TRADEOFF, POSITIVE, QUEST}
 
+
 export(int) var id = 0
 export(String) var name = ""
+export(String) var text = ""
 export(Type) var type = Type.LUCK
 export(Dictionary) var floor_appearance = {1: true, 2: true, 3: true}
---
+++ project/game/event/EventDisplay.gd
@@ -0,0 +1,11 @@
+extends Control
+
+const IMAGES = [preload("res://assets/images/events/event_luck.png"),
+		preload("res://assets/images/events/event_challenge.png"),
+		preload("res://assets/images/events/event_tradeoff.png"),
+		preload("res://assets/images/events/event_positive.png"),
+		preload("res://assets/images/events/event_quest.png")]
+
+
+func _ready():
+	pass

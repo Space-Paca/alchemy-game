28efa18362f5ab34993843f721e6f3aa30e51e5a
Rica <pudimdefrutas@gmail.com>
2020-10-30 17:37:48 -0300

    Updated version

--- project/game/debug-menu/Debug.gd
+++ project/game/debug-menu/Debug.gd
@@ -10,7 +10,7 @@ signal combinations_unlocked
 signal battle_won
 signal floor_selected(floor_number)
 
-const VERSION := "v.0.0.6"
+const VERSION := "v.0.1.0"
 const MAX_FLOOR := 3
 
 var floor_to_go := -1

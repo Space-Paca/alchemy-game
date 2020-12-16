1c2793bf4c196c404397d9a61897894cf73e3db8
ArthurTui <arthur.vb@gmail.com>
2020-12-16 19:02:27 -0300

    Add audio filter to pause

--- project/game/pause/PauseScreen.gd
+++ project/game/pause/PauseScreen.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 onready var bg = $Background
 
+const AUDIO_FILTER = preload("res://game/pause/pause_audio_filter.tres")
+
 var paused := false
 
 
@@ -11,9 +13,17 @@ func _unhandled_input(event):
 
 
 func toggle_pause():
-	paused = !paused
-	get_tree().paused = paused
-	bg.visible = paused
+	set_pause(!paused)
+
+
+func set_pause(p: bool):
+	paused = p
+	get_tree().paused = p
+	bg.visible = p
+	if p:
+		AudioServer.add_bus_effect(0, AUDIO_FILTER)
+	else:
+		AudioServer.remove_bus_effect(0, 0)
 
 
 func _on_Resume_pressed():
@@ -21,7 +31,7 @@ func _on_Resume_pressed():
 
 
 func _on_Return_pressed():
-	get_tree().paused = false
+	set_pause(false)
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://game/main-menu/MainMenu.tscn")
 

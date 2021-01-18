805573e7e94b080fa2178762d927cc1c1386563b
ArthurTui <arthur.vb@gmail.com>
2021-01-18 17:52:55 -0300

    Change map line dots and empty icon

--- project/game/map/MapLine.gd
+++ project/game/map/MapLine.gd
@@ -5,12 +5,16 @@ signal filled
 
 const BALL_COLOR = Color.black
 const BALL_DIST = 30
-const BALL_RADIUS = 5
+const BALL_SIZE = Vector2(10, 10)
 const MAP_NODE_SIZE = 64
 const PATH_FILL_TIME = 1
 const WAIT_TIMER = .1
+const IMAGES = [preload("res://assets/images/map/path-map-1.png"),
+	preload("res://assets/images/map/path-map-2.png"),
+	preload("res://assets/images/map/path-map-3.png")]
 
 var ball_amount : int
+var balls := []
 var line_vector : Vector2
 var time := 0.0
 
@@ -29,6 +33,8 @@ func set_line(origin:Vector2, target:Vector2):
 	ball_amount = int(dist / BALL_DIST) + 1
 	dist -= (ball_amount - 1) * BALL_DIST
 	position = origin + line_vector.clamped((MAP_NODE_SIZE + dist) / 2)
+	for i in ball_amount:
+		balls.append(IMAGES[randi() % IMAGES.size()])
 
 
 func _process(delta):
@@ -45,4 +51,5 @@ func _draw():
 		var pos := line_vector.clamped(i * BALL_DIST)
 		var radius_multiplier = ball_amount * time / PATH_FILL_TIME - i
 		radius_multiplier = clamp(radius_multiplier, 0, 1)
-		draw_circle(pos, BALL_RADIUS * radius_multiplier, BALL_COLOR)
+		var size = BALL_SIZE * radius_multiplier
+		draw_texture_rect(balls[i], Rect2(pos - size / 2, size), false)

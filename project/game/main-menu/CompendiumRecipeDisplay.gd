fe9ffb4c20f615ccd61468a08d579ba4381319b3
ArthurTui <arthur.vb@gmail.com>
2021-04-27 18:43:03 -0300

    Begin recipe compendium

---
+++ project/game/main-menu/CompendiumRecipeDisplay.gd
@@ -0,0 +1,95 @@
+extends Control
+
+signal hovered(reagent_array)
+signal unhovered()
+
+onready var bg = $Background
+onready var middle_container = $Background/MarginContainer/VBoxContainer/HBoxContainer/Middle
+onready var right_container = $Background/MarginContainer/VBoxContainer/HBoxContainer/Right
+onready var description = $Background/MarginContainer/VBoxContainer/Description
+onready var favorite_button = $Background/FavoriteButton
+onready var memorization_progress = $Background/MemorizationProgress
+onready var memorization_label = $Background/MemorizationLabel
+onready var grid = $Background/MarginContainer/VBoxContainer/HBoxContainer/Left/GridContainer
+onready var title = $Background/MarginContainer/VBoxContainer/Title
+onready var left_column = $Background/MarginContainer/VBoxContainer/HBoxContainer/Right/ReagentList/LeftColumn
+onready var right_column = $Background/MarginContainer/VBoxContainer/HBoxContainer/Right/ReagentList/RightColumn
+
+const REAGENT = preload("res://game/recipe-book/ReagentDisplay.tscn")
+const REAGENT_AMOUNT = preload("res://game/shop/ReagentAmount.tscn")
+const RECIPE_BG = preload("res://assets/images/ui/book/recipe_page.png")
+const RECIPE_MASTERED_BG = preload("res://assets/images/ui/book/mastered_recipe_page.png")
+const MAX_REAGENT_COLUMN = 4
+const HOVERED_SCALE = 1.05
+const SCALE_SPEED = 5
+
+var combination : Combination
+var reagent_array := []
+var filtered := true
+var hovered := false
+
+
+func _process(delta):
+	if hovered:
+		rect_scale.x = min(rect_scale.x + delta*SCALE_SPEED, HOVERED_SCALE)
+		rect_scale.y = min(rect_scale.y + delta*SCALE_SPEED, HOVERED_SCALE)
+	else:
+		rect_scale.x = max(rect_scale.x - delta*SCALE_SPEED, 1)
+		rect_scale.y = max(rect_scale.y - delta*SCALE_SPEED, 1)
+
+
+func set_combination(_combination: Combination):
+	combination = _combination
+	reagent_array = combination.recipe.reagents
+	title.text = combination.recipe.name
+	description.text = RecipeManager.get_description(combination.recipe)
+	grid.columns = combination.grid_size
+	
+	for i in range(combination.grid_size):
+		for j in range(combination.grid_size):
+			var reagent = REAGENT.instance()
+			reagent.set_mode("grid")
+			grid.add_child(reagent)
+			reagent.set_reagent(combination.known_matrix[i][j])
+	
+	if combination.discovered:
+		right_container.queue_free()
+		middle_container.queue_free()
+	else:
+		var columns := [left_column, right_column]
+		var i := 0
+		for reagent in combination.reagent_amounts:
+			var reagent_amount = REAGENT_AMOUNT.instance()
+# warning-ignore:integer_division
+			columns[i / MAX_REAGENT_COLUMN].add_child(reagent_amount)
+			reagent_amount.set_reagent(reagent)
+			reagent_amount.set_amount(combination.reagent_amounts[reagent])
+			i += 1
+
+
+func enable_tooltips():
+	for reagent_amount in grid.get_children():
+		reagent_amount.enable_tooltips()
+	
+	if left_column:
+		for reagent_amount in left_column.get_children():
+			reagent_amount.enable_tooltips()
+	if right_column:
+		for reagent_amount in right_column.get_children():
+			reagent_amount.enable_tooltips()
+
+
+func disable_tooltips():
+	for reagent_amount in grid.get_children():
+		reagent_amount.disable_tooltips()
+
+
+func _on_Panel_mouse_entered():
+	hovered = true
+	AudioManager.play_sfx("hover_recipe_button")
+	emit_signal("hovered", reagent_array)
+
+
+func _on_Panel_mouse_exited():
+	hovered = false
+	emit_signal("unhovered")
---
+++ project/game/main-menu/RecipeCompendium.gd
@@ -0,0 +1,76 @@
+extends Control
+
+onready var recipe_grid : GridContainer = $Background/ScrollContainer/RecipeGrid
+onready var scroll : ScrollContainer = $Background/ScrollContainer
+onready var filter_menu = $Background/FilterMenu
+
+const RECIPE = preload("res://game/main-menu/CompendiumRecipeDisplay.tscn")
+
+var recipe_displays := {}
+
+
+func _ready():
+	populate()
+
+
+func populate():
+	for recipe_name in Profile.known_recipes:
+		var display = RECIPE.instance()
+		var combination = Combination.new()
+		combination.create_from_recipe(RecipeManager.recipes[recipe_name], {})
+		recipe_grid.add_child(display)
+		display.set_combination(combination)
+		recipe_displays[recipe_name] = display
+
+
+func enable_tooltips():
+	for display in recipe_grid.get_children():
+		if display.visible:
+			display.enable_tooltips()
+		else:
+			display.disable_tooltips()
+
+
+func disable_tooltips():
+	for display in recipe_grid.get_children():
+		display.disable_tooltips()
+
+
+func filter_combinations(filters: Array):
+	for recipe_display in recipe_displays.values():
+		var filtered = true
+		for filter in filters:
+			if not filter in recipe_display.combination.recipe.filters:
+				filtered = false
+				break
+		recipe_display.filtered = filtered
+	
+	update_recipes_shown()
+
+
+func update_recipes_shown():
+	for recipe_display in recipe_displays.values():
+		recipe_display.visible = recipe_display.filtered
+	if visible:
+		enable_tooltips()
+
+
+func reset_recipe_visibility():
+	filter_menu.clear_filters()
+	
+	for recipe_display in recipe_displays.values():
+		recipe_display.filtered = true
+		recipe_display.show()
+
+
+func _on_FilterMenu_filters_updated(filters: Array):
+	filter_combinations(filters)
+
+
+func _on_button_mouse_entered():
+	AudioManager.play_sfx("hover_button")
+
+
+func _on_CloseButton_pressed():
+	pass
+	#close

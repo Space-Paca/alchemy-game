33b1f6f7c99e9809047d7ed289ed8123bab7394d
Ricardo Lira <pudimdefrutas@gmail.com>
2021-05-25 18:11:28 -0300

    Progress on xp sliders

--- project/game/ui/endgame-stats/EndGameXPProgressControl.gd
+++ project/game/ui/endgame-stats/EndGameXPProgressControl.gd
@@ -2,6 +2,9 @@ extends Control
 
 signal changed_xp
 
+const MODIFIED_COLOR = Color.palegreen
+const NORMAL_COLOR = Color.white
+
 onready var stat_name = $Name
 onready var current_xp_node = $NumberContainer/Current
 onready var max_xp_node = $NumberContainer/Max
@@ -12,7 +15,12 @@ var available_xp = 10
 var initial_xp = 20
 var modified_xp = 0
 var max_xp = 100
-var previous_value #USE THIS
+
+
+func _ready():
+	#TEST
+	setup("Unlock Recipes", 20, 100, 10)
+	set_available_xp(5)
 
 
 func setup(name, _initial_xp, _max_xp, total_available_xp):
@@ -27,22 +35,28 @@ func setup(name, _initial_xp, _max_xp, total_available_xp):
 	slider.max_value = total_available_xp
 
 
+func set_available_xp(value):
+	available_xp = value
+
 func update_xp_text():
 	current_xp_node.text = str(initial_xp + modified_xp)
 	if modified_xp > 0:
-		current_xp_node.modulate = Color.blue
+		allocated_label.modulate = MODIFIED_COLOR
+		current_xp_node.modulate = MODIFIED_COLOR
 	else:
-		current_xp_node.modulate = Color.white
+		allocated_label.modulate = NORMAL_COLOR
+		current_xp_node.modulate = NORMAL_COLOR
 	max_xp_node.text = "/"+str(max_xp)
 
 
 func _on_HSlider_value_changed(value):
 	if value > available_xp:
-		slider.value -= value - available_xp
-		value = value - available_xp
-		
+		var diff = value - available_xp
+		slider.set_value(slider.value - diff)
+		value = available_xp
 	allocated_label.text = str(slider.value)
-	if value != 0:
-		modified_xp += value
+	var changed_value = value - modified_xp
+	if changed_value != 0:
+		modified_xp += changed_value
 		update_xp_text()
 		emit_signal("changed_xp", value)
---
+++ project/game/ui/endgame-stats/XPControlSlider.gd
@@ -0,0 +1,10 @@
+extends HSlider
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+func set_value(v):
+	value = v

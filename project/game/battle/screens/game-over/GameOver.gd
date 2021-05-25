8a8eb468a0fa8ce06728d80db274e009554855b6
ArthurTui <arthur.vb@gmail.com>
2021-05-25 17:10:46 -0300

    Add buttons to change scene in memorization progress

--- project/game/battle/screens/game-over/GameOver.gd
+++ project/game/battle/screens/game-over/GameOver.gd
@@ -18,18 +18,6 @@ func animate_book():
 	tween.start()
 
 
-func _on_Button_pressed():
-	Transition.transition_to("res://game/main-menu/MainMenu.tscn")
-
-
-func _on_Button_button_down():
-	AudioManager.play_sfx("click")
-
-
-func _on_Button_mouse_entered():
-	AudioManager.play_sfx("hover_button")
-
-
 func _on_AnimationPlayer_animation_finished(_anim_name):
 	Transition.begin_transition()
 	yield(Transition, "screen_dimmed")
--- project/game/ui/endgame-stats/EndgameStats.gd
+++ project/game/ui/endgame-stats/EndgameStats.gd
@@ -1,5 +1,8 @@
 extends Control
 
+onready var menu_button = $Buttons/Menu
+onready var restart_button = $Buttons/Restart
+
 var player : Player
 
 
@@ -10,3 +13,31 @@ func _ready():
 func set_player(p: Player):
 	player = p
 	$CompendiumProgress.set_player(p)
+
+
+func progress_buttons_set_disabled(d: bool):
+	menu_button.disabled = d
+	restart_button.disabled = d
+
+
+func _on_Menu_pressed():
+	Transition.begin_transition()
+	yield(Transition, "screen_dimmed")
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://game/main-menu/MainMenu.tscn")
+	Transition.end_transition()
+
+func _on_Restart_pressed():
+	Transition.begin_transition()
+	yield(Transition, "screen_dimmed")
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://game/dungeon/Dungeon.tscn")
+	Transition.end_transition()
+
+
+func _on_Button_button_down():
+	AudioManager.play_sfx("click")
+
+
+func _on_Button_mouse_entered():
+	AudioManager.play_sfx("hover_button")

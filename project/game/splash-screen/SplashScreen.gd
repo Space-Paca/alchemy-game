903063954f8a618ac31749f3cef7be4ba2587f01
Ricardo Lira <pudimdefrutas@gmail.com>
2021-07-01 18:29:47 -0300

    Tweak on splash screen

--- project/game/splash-screen/SplashScreen.gd
+++ project/game/splash-screen/SplashScreen.gd
@@ -5,6 +5,7 @@ func _ready():
 	yield(get_tree(), "idle_frame")
 	Debug.set_version_visible(true)
 	if Profile.get_option("choose_language"):
+		$ChooseLanguage.hide()
 		yield(get_tree().create_timer(1.0), "timeout")
 		Transition.transition_to("res://game/main-menu/MainMenu.tscn")
 	else:

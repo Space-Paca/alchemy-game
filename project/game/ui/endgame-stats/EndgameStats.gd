0e9373188e689159537ffed25be9edd857551dcc
ArthurTui <arthur.vb@gmail.com>
2021-08-15 20:52:39 -0300

    Show spend xp warning

--- project/game/ui/endgame-stats/EndgameStats.gd
+++ project/game/ui/endgame-stats/EndgameStats.gd
@@ -5,10 +5,15 @@ onready var restart_button = $Page2/Buttons/Restart
 onready var xpdivider = $Page2/XPDivider
 onready var compendium_progress = $Page2/CompendiumProgress
 onready var unknown_compendium = $Page2/Unknown
+onready var xpwarning = $Page2/XPWarning
+onready var particle_container = $Page2/XPDivider/HBoxContainer/Amount/ParticleContainer
 
 const SPEED = 3
 
 var player : Player
+var is_menu_hovered := false
+var is_restart_hovered := false
+var xp_divider_animation_complete := false
 
 
 func _ready():
@@ -27,6 +32,15 @@ func _process(dt):
 	else:
 		unknown_compendium.modulate.a = max(unknown_compendium.modulate.a - dt*SPEED, 0.0)
 		compendium_progress.modulate.a = min(compendium_progress.modulate.a + dt*SPEED, 1.0)
+	
+	if xp_divider_animation_complete and\
+			((is_menu_hovered and menu_button.disabled) or\
+			(is_restart_hovered and restart_button.disabled)):
+		xpwarning.modulate.a = lerp(xpwarning.modulate.a, 1, .1)
+		particle_container.modulate.a = lerp(xpwarning.modulate.a, 1, .1)
+	else:
+		xpwarning.modulate.a = lerp(xpwarning.modulate.a, 0, .1)
+		particle_container.modulate.a = lerp(xpwarning.modulate.a, 0, .1)
 
 func set_player(p: Player):
 	player = p
@@ -62,6 +76,7 @@ func _on_Next_pressed():
 	#xpdivider.set_initial_xp_pool(10000)
 	xpdivider.set_initial_xp_pool($Page1/RunStats.total_xp)
 	yield(xpdivider, "setup_animation_complete")
+	xp_divider_animation_complete = true
 	progress_buttons_set_disabled(xpdivider.can_apply_xp())
 
 
@@ -89,3 +104,19 @@ func _on_XPDivider_content_unlocked(unlock_data):
 func _on_UnlockablePopup_closed():
 	$UnlockablePopup.hide()
 	$Page2/XPDivider.emit_signal("unlock_popup_closed")
+
+
+func _on_Restart_mouse_entered():
+	is_restart_hovered = true
+
+
+func _on_Menu_mouse_entered():
+	is_menu_hovered = true
+
+
+func _on_Restart_mouse_exited():
+	is_restart_hovered = false
+
+
+func _on_Menu_mouse_exited():
+	is_menu_hovered = false

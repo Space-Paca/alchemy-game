afe3978a0b29c09bf417c6ba4ab8924e60794a99
ArthurTui <arthur.vb@gmail.com>
2021-08-17 22:50:39 -0300

    Add animations to event text

--- project/game/event/EventDisplay.gd
+++ project/game/event/EventDisplay.gd
@@ -8,6 +8,7 @@ onready var title_label = $Title
 onready var text_label = $VBox/TextRect/TextContainer/Text
 onready var vbox = $VBox
 onready var image = $Image
+onready var tween = $Tween
 
 const THEME = preload("res://assets/themes/event_theme/event_theme.tres")
 # Text effects
@@ -21,6 +22,7 @@ const FORMAT_DICT = {
 		"(wave)": "[wave amp=50 freq=2]",
 		"(/wave)": "[/wave]"
 }
+const TEXT_SPEED = 150
 
 var event : Event
 var map_node : MapNode
@@ -33,6 +35,8 @@ func _ready():
 	EventManager.connect("spawned_battle", self, "_on_event_spawned_battle")
 # warning-ignore:return_value_discarded
 	EventManager.connect("spawned_rest", self, "_on_event_spawned_rest")
+# warning-ignore:return_value_discarded
+#	Transition.connect("finished", self, "_on_Transition_finished")
 
 
 func set_map_node(node: MapNode) -> void:
@@ -46,6 +50,7 @@ func load_event(new_event: Event, player: Player, override_text: String = ""):
 		text_label.bbcode_text = translate_and_format(override_text)
 	else:
 		text_label.bbcode_text = translate_and_format(event.text)
+	text_label.percent_visible = 0
 	image.texture = EventManager.IMAGES[event.type]
 	
 	for child in vbox.get_children():
@@ -60,6 +65,17 @@ func load_event(new_event: Event, player: Player, override_text: String = ""):
 		button.connect("pressed", EventManager, option.callback,
 				[self, player] + option.args)
 		button.theme = THEME
+		button.disabled = true
+		button.modulate.a = 0
+	
+	tween.stop_all()
+	
+	if Transition.active:
+		yield(Transition, "finished")
+	else:
+		yield(get_tree(), "idle_frame")
+	
+	animate_text(text_label.get_total_character_count() / TEXT_SPEED)
 
 
 func translate_and_format(text: String) -> String:
@@ -69,6 +85,25 @@ func translate_and_format(text: String) -> String:
 			text = text.replace(key, FORMAT_DICT[key])
 	
 	return text
+
+
+func animate_text(duration: float):
+	if text_label.percent_visible:
+		return
+	tween.interpolate_property(text_label, "percent_visible", 0, 1, duration)
+	tween.start()
+	
+	yield(tween, "tween_completed")
+	
+	for button in vbox.get_children():
+		if button is Button:
+			tween.interpolate_property(button, "modulate:a", 0, 1, .5)
+			tween.start()
+			yield(tween, "tween_completed")
+	
+	for button in vbox.get_children():
+		if button is Button:
+			button.disabled = false
 
 
 func _on_event_left():

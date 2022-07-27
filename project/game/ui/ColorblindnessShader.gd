32da17e775fe6fa9c371470370e2e21fc73a6d88
Ricardo Lira <pudimdefrutas@gmail.com>
2022-07-27 03:04:39 -0300

    Colorblindness smooth intensity

--- project/game/ui/ColorblindnessShader.gd
+++ project/game/ui/ColorblindnessShader.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 enum COLORBLIND_MODES {PROTANOPIA, DEUTRANOPIA, TRITANOPIA}
 
 const MODES_NAME =  ["PROTANOPIA", "DEUTRANOPIA", "TRITANOPIA"]
+const SPEED = 2.5
+
 
 func set_mode(value):
 	assert(value >= 0 and value < COLORBLIND_MODES.size(), "Not a valid colorblind mode: " + str(value))
@@ -10,8 +12,18 @@ func set_mode(value):
 
 
 func enable():
-	$Shader.material.set_shader_param("intensity", 1.0)
+	var cur = $Shader.material.get_shader_param("intensity")
+	if cur < 1.0:
+		$Tween.remove_all()
+		$Tween.interpolate_property($Shader.material, "shader_param/intensity",\
+									null, 1.0, (1.0 - cur)/SPEED, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		$Tween.start()
 
 
 func disable():
-	$Shader.material.set_shader_param("intensity", 0.0)
+	var cur = $Shader.material.get_shader_param("intensity")
+	if cur > 0.0:
+		$Tween.remove_all()
+		$Tween.interpolate_property($Shader.material, "shader_param/intensity",\
+									null, 0.0, cur/SPEED, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		$Tween.start()

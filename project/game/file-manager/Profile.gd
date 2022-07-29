eff09f8ba3b7e6cc2cb7714a235160bee0c8c72b
Ricardo Lira <pudimdefrutas@gmail.com>
2022-07-29 17:17:09 -0300

    Added bigger window sizes + fixed tabs on settings screen

--- project/game/file-manager/Profile.gd
+++ project/game/file-manager/Profile.gd
@@ -1,7 +1,8 @@
 extends Node
 
 const WINDOW_SIZES = [Vector2(1920, 1080), Vector2(1600, 900),
-		Vector2(1366, 768), Vector2(1280, 720)]
+		Vector2(1366, 768), Vector2(1280, 720), Vector2(2560, 1440), 
+		Vector2(3840, 2160)]
 const MAX_MEMORIZATION_LEVEL = 4
 const LANGUAGES = [
 	{"locale":"en", "name": "English"},

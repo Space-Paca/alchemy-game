4c5947fe8f6c340f4475c0d2651fb3d5352af9f6
Ricardo Lira <pudimdefrutas@gmail.com>
2022-08-01 13:16:43 -0300

    Nerfed dodge enemy

--- project/database/enemies/Dodger.gd
+++ project/database/enemies/Dodger.gd
@@ -5,7 +5,7 @@ var image = "res://assets/images/enemies/dodge enemy/idle.png"
 var name = "EN_DODGER"
 var sfx = "nimble"
 var use_idle_sfx = false
-var hp = 28
+var hp = 21
 var battle_init = true
 var size = "small"
 var change_phase = null
@@ -14,11 +14,10 @@ var states = ["init", "dodge", "attack", "big_attack"]
 var connections = [	      ["init", "big_attack", 1],
 						  ["big_attack", "dodge", 1],
 						  ["big_attack", "attack", 5],
-						  ["dodge", "attack", 6],
-						  ["dodge", "big_attack", 3],
-						  ["attack", "dodge", 3],
-						  ["attack", "attack", 5],
-						  ["attack", "big_attack", 2],
+						  ["dodge", "big_attack", 1],
+						  ["attack", "attack", 2],
+						  ["attack", "dodge", 1],
+						  ["attack", "big_attack", 7],
 				 ]
 var first_state = ["init"]
 

faaabbb674162a01e9ed1fb1a255259a28d1054b
Ricardo Lira <pudimdefrutas@gmail.com>
2022-11-08 18:12:28 -0300

    Bump to 0.8.1

--- project/game/debug-menu/Debug.gd
+++ project/game/debug-menu/Debug.gd
@@ -21,7 +21,7 @@ signal artifact_pressed(name)
 signal damage_all
 signal recipe_simulated(recipe)
 
-const VERSION := "v0.8.1dev"
+const VERSION := "v0.8.1"
 const MAX_FLOOR := 3
 const PORTRAITS_PATH = "res://assets/images/ui/book/portraits/"
 
